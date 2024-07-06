3e5d576a55013323db7387b61364afa3d38829f4
Mingzhao Zhou <edwardwhite7@hotmail.com>
2024-07-06 20:58:53 +0100

    dynamic html

--- 4-web-server/todoapp/todo/urls.py
+++ 4-web-server/todoapp/todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, AboutView
+from .views import index, AboutView, task
 
 urlpatterns = [
   # path(path to visit, function to run when path is loaded,  name to reference a path)
   path('', index, name='index'),
-  path('about/', AboutView.as_view(), name='about')
+  path('about/', AboutView.as_view(), name='about'),
+  path('task/<int:id>', task, name='task')
 ]
--- 4-web-server/todoapp/todo/views.py
+++ 4-web-server/todoapp/todo/views.py
@@ -4,11 +4,35 @@ from django.views import View
 
 # Create your views here.
 
+
+tasks = [
+  {"id": 1, "name": "Make video", "description":"make vid for course"},
+  {"id": 2, "name": "Do dishes", "description":"wash dishes"},
+  {"id": 3, "name": "Call mum", "description":"call mum fast"},
+]
+
+def get_task_by_id(task_id):
+  for task in tasks:
+    if task["id"] == task_id:
+      return task
+    return None
+
 def index(request):
-  # return HttpResponse('<h1>Todo</h1>')
-  return render(request, 'todo/index.html')
+  # return HttpResponse("<h1>Todo</h1>")
+  # tasks2 = []
+  ctx = {
+    "tasks": tasks
+  }
+
+  return render(request, "todo/index.html", ctx)
+
+def task(request, id):
+  ctx = {
+    "task": get_task_by_id(id)
+  }
+  return render(request, "todo/task.html", ctx)
 
 # AboutView is the subclass of view
 class AboutView(View):
   def get(self, request):
-    return HttpResponse('This is the about page')
+    return HttpResponse("This is the about page")
