7d559c3f7423c17057ffb4abf2e728e76fd6a4eb
Mingzhao Zhou <edwardwhite7@hotmail.com>
2024-07-06 12:27:43 +0100

    add code

---
+++ 4-web-server/todo-app/script.js
@@ -0,0 +1,42 @@
+// document.addEventListener("DOMContentLoaded", function () {});
+
+// document.getElementById("addButton").addEventListener("click", function () {});
+
+const input = document.getElementById("input");
+const priority = document.getElementById("priority");
+const listsContainer = document.getElementById("lists-container");
+
+function addTask() {
+  const task = input.value;
+  if (task.trim() === "") {
+    return;
+  }
+  const taskPriority = priority.value;
+  const taskItem = document.createElement("li");
+  const taskText = document.createElement("span");
+  taskText.classList.add("task-text");
+
+  const checkbox = document.createElement("input");
+  checkbox.classList.add("form-check-input");
+  checkbox.type = "checkbox";
+
+  taskText.textContent = task;
+  taskItem.appendChild(checkbox);
+  taskItem.appendChild(taskText);
+  const taskList = document.getElementById("task-list-" + taskPriority);
+
+  taskList.appendChild(taskItem);
+  input.value = "";
+}
+
+listsContainer.addEventListener("click", checkBoxClick);
+
+function checkBoxClick(event) {
+  console.log(event);
+
+  if (event.target.tagName === "INPUT" && event.target.type === "checkbox") {
+    const listItem = event.target.parentNode;
+    const taskList = listItem.parentNode;
+    taskList.removeChild(listItem);
+  }
+}
